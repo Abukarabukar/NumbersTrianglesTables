0f6df56fa28114b7b23f394a54fd93bd31834200
Abukarabukare <Abukarshegow@gmail.comm>
2024-03-06 08:31:10 -0500

    Completed

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,14 +2,50 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+// define the number
+        int size = 5;
+        // table builder
+        StringBuilder table = new StringBuilder();
+//loop
+        for (int i=1; i <= size; i++) {
+            for (int j = 1; j <=size; j++){
+               table.append(String.format("%3d |", i * j));
+            }
+            table.append("\n");
+            // return to string
+        }
+    return table.toString();
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        // define the number
+        int size = 10;
+        // table builder
+        StringBuilder table = new StringBuilder();
+//loop
+        for (int i=1; i <= size; i++) {
+            for (int j = 1; j <=size; j++){
+                table.append(String.format("%3d |", i * j));
+            }
+            table.append("\n");
+            // return to string
+        }
+        return table.toString();
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        // define the number
+        int size = 20;
+        // table builder
+        StringBuilder table = new StringBuilder();
+//loop
+        for (int i=1; i <= size; i++) {
+            for (int j = 1; j <=size; j++){
+                table.append(String.format("%3d |", i * j));
+            }
+            table.append("\n");
+            // return to string
+        }
+        return table.toString();
     }
 }
--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -3,18 +3,70 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        // StringBuilder
+        StringBuilder tree = new StringBuilder();
+
+        // Outer loop to create each row of the
+        for (int row = 1; row < numberOfRows; row++) {
+
+            // Inner loop to append stars (*) to the string
+            for (int star = 1; star <= row; star++) {
+                tree.append("*");
+            }
+
+            // Append a newline
+            tree.append("\n");
+        }
+        return tree.toString();
+
     }
 
-    public static String getRow(int numberOfStars) {
-        return null;
+
+    public static String getRow(int width) {
+        // StringBuilder
+        StringBuilder tree = new StringBuilder();
+
+        // Outer loop to create each row of the
+        for (int i = 0; i < width; i++) {
+                tree.append("*");
+            }
+
+        return tree.toString();
     }
 
     public static String getSmallTriangle() {
-        return null;
+        // StringBuilder
+        StringBuilder tree = new StringBuilder();
+        int stars = 5;
+        // Outer loop to create each row of the
+        for (int row = 1; row < stars; row++) {
+
+            // Inner loop to append stars (*) to the string
+            for (int star = 1; star <= row; star++) {
+                tree.append("*");
+            }
+
+            // Append a newline
+            tree.append("\n");
+        }
+        return tree.toString();
     }
 
     public static String getLargeTriangle() {
-        return null;
+        // StringBuilder
+        StringBuilder tree = new StringBuilder();
+        int stars = 10;
+        // Outer loop to create each row of the
+        for (int row = 1; row < stars; row++) {
+
+            // Inner loop to append stars (*) to the string
+            for (int star = 1; star <= row; star++) {
+                tree.append("*");
+            }
+
+            // Append a newline
+            tree.append("\n");
+        }
+        return tree.toString();
     }
 }
